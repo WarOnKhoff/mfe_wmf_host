9b23fbc3a78f7c031f1103324dfdbfb42cb6e580
Alexander Voronkov <voronkovs.aleksadrs@gmail.com>
2023-05-09 00:21:21 +0300

    Added Lazy loading

--- src/App.jsx
+++ src/App.jsx
@@ -1,17 +1,27 @@
 import React, { Suspense } from "react"
 import ReactDOM from "react-dom"
 import Header from "header/Header"
-import Dashboard from "dashboard/Dashboard"
-import Details from "details/Details"
+// import Dashboard from "dashboard/Dashboard"
+// import Details from "details/Details"
 import { AppContextProvider } from "host/store"
 
 import "./index.css"
 
+const HeaderComponent = React.lazy(() => import("./Header"))
+
 const App = () => (
-	<AppContextProvider>
-		<Header />
-		<Dashboard />
-		<Details />
-	</AppContextProvider>
+	<>
+		<Suspense fallback={<div>Loading...</div>}>
+			<HeaderComponent />
+		</Suspense>
+		{/* <Dashboard />
+		<Details /> */}
+	</>
 )
-ReactDOM.render(<App />, document.getElementById("app"))
+
+ReactDOM.render(
+	<AppContextProvider>
+		<App />
+	</AppContextProvider>,
+	document.getElementById("app")
+)
